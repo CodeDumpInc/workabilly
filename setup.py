473671b083d86bffe3ea3e2a8f90e05254514dd5
weeska <weeska@gmail.com>
2016-02-21 14:27:02 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='workabilly',
+    packages=['workabilly'],
+    version='0.1',
+    description='Modular build configurations',
+    author='Philipp Böhnke',
+    author_email='weeska@gmail.com',
+    url='',
+    download_url='',
+    keywords=['configuration', 'build'],
+    classifiers=[],
+)
