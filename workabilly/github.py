bccb7c8468d1a1f0d2b4b834c3411b18d70198f1
weeska <weeska@gmail.com>
2016-02-21 19:37:27 +0100

    Add downloader used by githubgrabber

--- workabilly/github.py
+++ workabilly/github.py
@@ -1,19 +1,26 @@
-from .base.workers import BaseWorker
+from .downloader import Downloader
 
-import os
 import shutil
-import wget
 
 
-class GithubArchiveGrabber(BaseWorker):
+class GithubArchiveGrabber(Downloader):
+
+    def _buildUrl(self):
+        archive = self.tag
+        if archive is None:
+            archive = self.branch
+
+        format_str = ("https://github.com/%s/%s/archive/%s.zip")
+        return format_str % (self.user, self.project, archive)
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
         self.user = kwargs.get('user')
         self.project = kwargs.get('project')
         self.tag = kwargs.get('tag')
         self.branch = kwargs.get('branch', 'master')
         self.target = kwargs.get('target', './')
+
+        super().__init__(source=self._buildUrl(), target=self.target)
 
     def preExecuteDescription(self):
         return 'Creating Temporary Folder ' + self.target
@@ -24,22 +31,8 @@ class GithubArchiveGrabber(BaseWorker):
     def executeDescription(self):
         return 'Downloading ' + self.project
 
-    def _buildUrl(self):
-        archive = self.tag
-        if archive is None:
-            archive = self.branch
-
-        format_str = ("https://github.com/%s/%s/archive/%s.zip")
-        return format_str % (self.user, self.project, archive)
-
-    def preExecute(self):
-        if not os.path.exists(self.target):
-            os.makedirs(self.target)
-
     def execute(self, **kwargs):
-        url = self._buildUrl()
-        self.archive = wget.download(url, out=self.target)
-        return {'archive': self.archive}
+        return {'archive': super().execute(**kwargs).get('downloaded')}
 
     def postExecute(self):
         shutil.rmtree(self.target)
