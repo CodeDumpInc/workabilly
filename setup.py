ec09bc39f67dd9d74425f619e1a39b3c77139320
weeska <weeska@gmail.com>
2016-03-04 23:07:34 +0100

    Remove gitgrabber example

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='workabilly',
     packages=['workabilly', 'workabilly.base'],
-    version='0.1.1',
+    version='0.1.2',
     description='Tools to get a project set up',
     author='weeska',
     author_email='weeska@gmail.com',
